cb4f777b361af2125aaf94ec6b0cef24fcfe89d7
Khaled Ghareeb <khaledghareeb147@gmail.com>
2026-09-14 15:44:19 +0300

    f2

--- deploy/app.py
+++ deploy/app.py
@@ -432,7 +432,7 @@ st.markdown(css, unsafe_allow_html=True)
 st.markdown(
     """
     <div style="display: flex; align-items: center; justify-content: center; gap: 15px; margin-bottom: 5px;">
-        <img src="https://resources.premierleague.com/premierleague/badges/50/t-default.png" width="48" style="vertical-align: middle;">
+        <img src="https://upload.wikimedia.org/wikipedia/en/f/f2/Premier_League_Logo.svg" width="48" style="vertical-align: middle; filter: brightness(0) invert(1);">
         <h1 id="title" style="margin: 0; line-height: 1;">Premier League Match Predictor</h1>
     </div>
     """,
